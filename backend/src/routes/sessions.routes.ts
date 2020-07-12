e99536a9363cb317afc33e20ef979a52eea9e797
otaviopetry <otaviopetry@gmail.com>
2020-07-12 00:32:21 -0300

    fix: Code commenting on Authenticate feature

--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -6,19 +6,25 @@ const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
     try {
+        // store email and password sent by user
         const { email, password } = request.body;
 
+        // create service instance
         const authenticateUser = new AuthenticateUserService();
 
+        // execute service passing informed data
         const { user } = await authenticateUser.execute({
             email,
             password,
         });
 
+        // if service is sucessful, delete user password from response
         delete user.password;
 
+        // return user found without password
         return response.json({ user });
     } catch (err) {
+        // if service throws error, send error message
         return response.status(400).json({ error: err.message });
     }
 });
--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -18,18 +18,23 @@ class AuthenticateUserService {
         // create repository instance
         const usersRepository = getRepository(User);
 
+        // check if user exists
         const user = await usersRepository.findOne({ where: { email } });
 
+        // if doesnt exist, throw error
         if (!user) {
             throw new Error('Incorrect email/password combination.');
         }
 
+        // check if informed password matches encrypted password using bcryptjs compare
         const passwordMatched = await compare(password, user.password);
 
+        // if doesnt match, throw error
         if (!passwordMatched) {
             throw new Error('Incorrect email/password combination.');
         }
 
+        // if matches, return user
         return {
             user,
         };
