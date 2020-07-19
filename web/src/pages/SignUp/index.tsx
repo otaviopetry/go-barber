b8f3db2f3b074c8bf838012fe18748ec4c3fa449
otaviopetry <otaviopetry@gmail.com>
2020-07-19 15:49:51 -0300

    feat (utils): forms getValidationError function
    Get all validation errors from Yup and return an object

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import { Container, Content, Background } from './styles';
 
@@ -11,23 +13,27 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Informe seu nome'),
         email: Yup.string()
           .required('Informe seu e-mail')
           .email('Seu e-mail está correto?'),
-        password: Yup.string()
-          .min(6, 'Escolha no mínimo 6 caracteres')
-          .required('Senha obrigatória'),
+        password: Yup.string().min(6, 'Escolha no mínimo 6 caracteres'),
       });
 
       await schema.validate(data, {
         abortEarly: false,
       });
     } catch (err) {
-      console.warn(err);
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -37,15 +43,10 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
-          <Input
-            type="text"
-            name="username"
-            icon={FiUser}
-            placeholder="Seu nome completo"
-          />
+          <Input type="text" name="name" icon={FiUser} placeholder="Nome" />
           <Input type="text" name="email" icon={FiMail} placeholder="E-mail" />
           <Input
             type="password"
