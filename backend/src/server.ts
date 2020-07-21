432d693f85e8e183f3c11f8181e6ccb358e5165b
otaviopetry <otaviopetry@gmail.com>
2020-07-21 01:11:17 -0300

    chore: add cors to api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -12,6 +13,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
