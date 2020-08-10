9cb74f6b52e1398d063e641e244668829d7b0fa9
otaviopetry <otaviopetry@gmail.com>
2020-08-10 12:00:32 -0300

    feat (web): Custom route component
    Check if user is logged in and if route is private

---
+++ web/src/routes/Route.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  RouteProps as ReactDOMRouteProps,
+  Route as ReactDOMRoute,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  // if isPrivate === user exists (true/true, false/false), return the desired component
+  // else, redirect to according screen (dashboard for false/true and login for true/false )
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
