dc58ddecc430106c4b863b7d7bf82b2dd9ca62ac
otaviopetry <otaviopetry@gmail.com>
2020-08-11 21:25:14 -0300

    feat (mobile): Declare png as module (@types)

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Text, StatusBar } from 'react-native';
+import { Text, StatusBar, Image } from 'react-native';
 
 import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="#eaeaea" />
       <Container>
+        <Image source={logoImg} />
         <Text style={{ color: '#fff', alignSelf: 'center', fontSize: 24 }}>
           Hello dude!
         </Text>
