4b36f1870df456ef640168bc7ff1d364e0d2c5d8
otaviopetry <otaviopetry@gmail.com>
2020-09-09 17:48:37 -0300

    feat(server): Authentication test
    It should not be able to authenticate with non-existing email

--- backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
@@ -30,5 +31,22 @@ describe('AuthenticateUser', () => {
 
         expect(response).toHaveProperty('token');
         expect(response.user).toEqual(user);
+    });
+
+    it('should not be able to authenticate with non-existing email', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeHashProvider();
+
+        const authenticateUser = new AuthenticateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
+
+        expect(
+            authenticateUser.execute({
+                email: 'johndoe@example.com',
+                password: '123456',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
     });
 });
