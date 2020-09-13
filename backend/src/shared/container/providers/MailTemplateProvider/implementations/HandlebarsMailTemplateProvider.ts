a18c0c8f7ad11f190dacb98f3ccde0539516ee03
otaviopetry <otaviopetry@gmail.com>
2020-09-13 18:57:46 -0300

    feat(server): Forgot password email template
    Handlebars template for Forgot password email service

--- backend/src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
+++ backend/src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -1,15 +1,21 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 
 import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
 
 class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
     public async parse({
-        template,
+        file,
         variables,
     }: IParseMailTemplateDTO): Promise<string> {
+        // get the template file from file system
+        const templateFileContent = await fs.promises.readFile(file, {
+            encoding: 'utf-8',
+        });
+
         // inside compile we can pass anything we want
-        const parseTemplate = handlebars.compile(template);
+        const parseTemplate = handlebars.compile(templateFileContent);
 
         // and here we call this parseTemplate function with the variables
         return parseTemplate(variables);
