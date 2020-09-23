462ec6d26b37c86fbca5d2ce4bc1a8265c5f96c3
otaviopetry <otaviopetry@gmail.com>
2020-09-23 20:03:18 -0300

    feat(server): Create appointment business logic
    Implementation of business rules in Create Appointment Service

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,5 +1,4 @@
-/* eslint-disable camelcase */
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -32,6 +31,22 @@ class CreateAppointmentService {
     }: IRequest): Promise<Appointment> {
         // force 1 hour blocks
         const appointmentDate = startOfHour(date);
+
+        if (isBefore(appointmentDate, Date.now())) {
+            throw new AppError(
+                "You can't create an appointment in a past date",
+            );
+        }
+
+        if (user_id === provider_id) {
+            throw new AppError("You can't create an appointment with yourself");
+        }
+
+        if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+            throw new AppError(
+                'You can only create an appointment between 8am and 5pm',
+            );
+        }
 
         // use the findByDate method created in appointments repository
         const findAppointmentConflict = await this.appointmentsRepository.findByDate(
