dff387755851f50e84d13b0a748d84a3ec5cf21a
otaviopetry <otaviopetry@gmail.com>
2020-09-23 18:01:33 -0300

    feat(server): Exclude vacant hours from the past
    Do not show vacant hours before current Date

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -24,6 +24,20 @@ describe('ListProviderDayAvailability', () => {
             date: new Date(2020, 4, 20, 10, 0, 0),
         });
 
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 14, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 15, 0, 0),
+        });
+
+        jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+            return new Date(2020, 4, 20, 11).getTime();
+        });
+
         // here we specify the month as the actual calendar number (may is 5, not 4 as above)
         const availability = await listProviderDayAvailability.execute({
             provider_id: 'user',
@@ -35,9 +49,15 @@ describe('ListProviderDayAvailability', () => {
         expect(availability).toEqual(
             expect.arrayContaining([
                 { hour: 8, available: false },
-                { hour: 9, available: true },
+                { hour: 9, available: false },
                 { hour: 10, available: false },
-                { hour: 11, available: true },
+                { hour: 11, available: false },
+                { hour: 12, available: true },
+                { hour: 13, available: true },
+                { hour: 14, available: false },
+                { hour: 15, available: false },
+                { hour: 16, available: true },
+                { hour: 17, available: true },
             ]),
         );
     });
