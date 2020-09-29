4b953517aed86d71be3e311a003808de12ac7c4e
otaviopetry <otaviopetry@gmail.com>
2020-09-29 18:32:06 -0300

    feat(server): Providers list cache
    Implements cache in List Providers service

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,26 +1,40 @@
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import { inject, injectable } from 'tsyringe';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import User from '@modules/users/infra/typeorm/entities/User';
 
 interface IRequest {
-    user_id: string;
+  user_id: string;
 }
 
 @injectable()
 class ListProvidersService {
-    constructor(
-        @inject('UsersRepository')
-        private usersRepository: IUsersRepository,
-    ) {}
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
 
-    public async execute({ user_id }: IRequest): Promise<User[]> {
-        const users = await this.usersRepository.findAllProviders({
-            except_user_id: user_id,
-        });
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) {}
 
-        return users;
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+
+      console.log('Query has been executed.');
+
+      await this.cacheProvider.save(`providers-list:${user_id}`, users);
     }
+
+    return users;
+  }
 }
 
 export default ListProvidersService;
