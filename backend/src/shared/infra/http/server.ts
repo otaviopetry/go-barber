95ffd098cfc861f761905fa009d84b95f2758138
otaviopetry <otaviopetry@gmail.com>
2020-09-30 20:30:35 -0300

    feat(server): Express rate limiter
    Prevent DDoS with node-rate-limiter-flexible

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import { errors } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -15,6 +16,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
@@ -25,25 +27,25 @@ app.use(errors());
 
 // Global exception handler (underline on last parameter is to eslint ignore unused var)
 app.use(
-    (error: Error, request: Request, response: Response, _: NextFunction) => {
-        // if it is a known error, return it
-        if (error instanceof AppError) {
-            return response.status(error.statusCode).json({
-                status: 'error',
-                message: error.message,
-            });
-        }
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    // if it is a known error, return it
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
 
-        console.error(error);
+    console.error(error);
 
-        // if its unknown, return generic error
-        return response.status(500).json({
-            status: 'error',
-            message: 'Internal server error.',
-        });
-    },
+    // if its unknown, return generic error
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error.',
+    });
+  },
 );
 
 app.listen(3333, () => {
-    console.log('🚀  Server has launched on port 3333, dude!');
+  console.log('🚀  Server has launched on port 3333, dude!');
 });
