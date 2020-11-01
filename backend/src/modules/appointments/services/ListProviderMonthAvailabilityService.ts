6597b8d25d10528a89e577e9e281938923aef350
otaviopetry <otaviopetry@gmail.com>
2020-11-01 06:35:11 -0300

    fix(server): Make past days unavailable

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,60 +1,63 @@
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
-import { getDate, getDaysInMonth } from 'date-fns';
+import { getDate, getDaysInMonth, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
-    provider_id: string;
-    month: number;
-    year: number;
+  provider_id: string;
+  month: number;
+  year: number;
 }
 
 type IResponse = Array<{
-    day: number;
-    available: boolean;
+  day: number;
+  available: boolean;
 }>;
 
 @injectable()
 class ListProviderMonthAvailabilityService {
-    constructor(
-        @inject('AppointmentsRepository')
-        private appointmentsRepository: IAppointmentsRepository,
-    ) {}
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
 
-    public async execute({
+  public async execute({
+    provider_id,
+    year,
+    month,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.checkProviderMonthAvailability(
+      {
         provider_id,
         year,
         month,
-    }: IRequest): Promise<IResponse> {
-        const appointments = await this.appointmentsRepository.checkProviderMonthAvailability(
-            {
-                provider_id,
-                year,
-                month,
-            },
-        );
+      },
+    );
 
-        const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-        const eachDayArray = Array.from(
-            { length: numberOfDaysInMonth },
-            (_, index) => index + 1,
-        );
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
 
-        const availability = eachDayArray.map(day => {
-            const appointmentsInDay = appointments.filter(appointment => {
-                return getDate(appointment.date) === day;
-            });
+    const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
 
-            return {
-                day,
-                available: appointmentsInDay.length < 10,
-            };
-        });
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
 
-        return availability;
-    }
+      return {
+        day,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
+  }
 }
 
 export default ListProviderMonthAvailabilityService;
