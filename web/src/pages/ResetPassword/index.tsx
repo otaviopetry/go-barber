5c3e4114cc44b76bd5186c9dbeac3a36bd6f7d6f
otaviopetry <otaviopetry@gmail.com>
2020-11-01 08:20:08 -0300

    fix(web): Get token from location

--- web/src/pages/ResetPassword/index.tsx
+++ web/src/pages/ResetPassword/index.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useCallback } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import { FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
@@ -15,6 +15,7 @@ import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import api from '../../services/api';
 
 interface ResetPasswordFormData {
   password: string;
@@ -26,6 +27,7 @@ const ResetPassword: React.FC = () => {
 
   const { addToast } = useToast();
   const history = useHistory();
+  const location = useLocation();
 
   const handleSubmit = useCallback(
     async (data: ResetPasswordFormData) => {
@@ -42,6 +44,19 @@ const ResetPassword: React.FC = () => {
 
         await schema.validate(data, {
           abortEarly: false,
+        });
+
+        const { password, password_confirmation } = data;
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
         });
 
         history.push('/');
@@ -62,7 +77,7 @@ const ResetPassword: React.FC = () => {
         });
       }
     },
-    [addToast, history],
+    [addToast, history, location.search],
   );
 
   return (
