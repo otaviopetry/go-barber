b8b504a6d1f77bb22ccfc6cc8e50d222218bfbed
otaviopetry <otaviopetry@gmail.com>
2021-02-23 01:50:35 -0300

    feat: Add Status Bar config

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import AppProvider from './hooks';
@@ -10,6 +10,11 @@ import Routes from './routes';
 const App: React.FC = () => {
   return (
     <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#312e38"
+        translucent
+      />
       <AppProvider>
         <View
           style={{
