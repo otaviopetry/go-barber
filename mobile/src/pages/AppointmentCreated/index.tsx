a3807c28728c130fad739607eb25e0b34398ad18
otaviopetry <otaviopetry@gmail.com>
2021-02-25 00:09:13 -0300

    feat: AppointmentCreated layout

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>
+        Terça, this january is time to michael down your vincents
+      </Description>
+
+      <OkButton>
+        <OkButtonText>Okay!</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
